de32f7b71e61f03b06b75263fb5e5e718c8ca869
jaime <jaime@vaio>
2016-10-10 18:45:04 -0500

    Fp-Menu version 2

--- src/problema1/Problema1.java
+++ src/problema1/Problema1.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package problema1;
+import java.util.*;
 
 /**
  *
@@ -15,7 +16,27 @@ public class Problema1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int anyo;
+        anyo = solicitarAnyo();
+        
+        
     }
-    
+     public static int solicitarDato(String d){
+        int miDato;
+        System.out.println("Introduce el año, para saber si es bisiesto o no: " + d);
+        Scanner teclado= new Scanner(System.in);
+        miDato=teclado.nextInt();
+        return miDato;
+     }
+        
+       public static int solicitarAnyo(){
+        int anyo;
+        anyo = solicitarDato("");
+        if (((400 % anyo == 0) && (100 % anyo != 0)) || (4 % anyo == 0)){
+    	System.out.println("El año es bisiesto");}
+        else{
+    	System.out.println("El año no es bisiesto");
+        }
+        return anyo;
+       }
 }
