4738dffa9639f10c7b5745097a5984bef82ea6e9
jaime <jaime@vaio>
2016-10-20 14:44:51 -0500

    problema 1

--- src/problema1/Problema1.java
+++ src/problema1/Problema1.java
@@ -16,27 +16,33 @@ public class Problema1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int anyo;
-        anyo = solicitarAnyo();
+       int anyo,resultado;
+        anyo=solicitarDato(); //solicitamos el dato
+        resultado = operacion(anyo);// se hace la operacion
+        mensajeSalida();
+
         
         
     }
-     public static int solicitarDato(String d){
-        int miDato;
-        System.out.println("Introduce el año, para saber si es bisiesto o no: " + d);
+     public static int solicitarDato(){
+        int anyo;
+        System.out.println("Introduce el año, para saber si es bisiesto o no, ;) : ");
         Scanner teclado= new Scanner(System.in);
-        miDato=teclado.nextInt();
-        return miDato;
+        anyo=teclado.nextInt();
+        return anyo;
      }
         
-       public static int solicitarAnyo(){
-        int anyo;
-        anyo = solicitarDato("");
-        if (((400 % anyo == 0) && (100 % anyo != 0)) || (4 % anyo == 0)){
-    	System.out.println("El año es bisiesto");}
-        else{
-    	System.out.println("El año no es bisiesto");
+       public static int operacion(int anyo){
+      
+        if ((anyo % 4 == 0) && ((anyo % 100 != 0) || (anyo % 400 == 0))){ //si el anyo se pude dividir entre 4 o entre 100 y 400 y no quede ressiduo
+            System.out.println("El año es bisiesto");   //entonces es bisiesto
         }
+        else{
+            System.out.println("El año no es bisiesto");}
         return anyo;
+       }
+       public static void mensajeSalida(){
+           System.out.println("Gracias por usar el programa"); // se agradece por usar el programa
+           System.exit(0);
        }
 }
